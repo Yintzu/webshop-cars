df48b949d431791e24b6d3cc723fc51f879b0171
Chris <to.chris.lindstrom@gmail.com>
2021-02-27 00:14:28 +0100

    Animation on cart update, with useEffect in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { ShoppingCartContext } from '../contexts/ShoppingCartContext';
 import style from '../css/Navbar.module.css';
@@ -7,6 +7,16 @@ const Navbar = () => {
     // Importing cart from ShoppingCartContext
     // Using its length in render of cart icon
     const { shoppingCartItems: cart } = useContext(ShoppingCartContext);
+    const [onCartUpdate, setOnCartUpdate] = useState(false);
+
+    // On change in cart, set onCartUpdate to true and then back to false after a short duration
+    // While true, will have css-class with animation
+    useEffect(() => {
+        setOnCartUpdate(true);
+        setTimeout(() => {
+            setOnCartUpdate(false);
+        }, 400)
+    }, [cart])
 
     return ( 
         <nav className={style.navbar}>
@@ -16,8 +26,8 @@ const Navbar = () => {
                 <NavLink className={style.links} activeClassName={style.active} exact to="/about">About</NavLink>
             </div> 
             <NavLink className={style.cartIcon} exact to="/checkout">
-                    {/* numOfItems will be displayed based on cart-state length, if 0 it won't be displayed at all */}
-                    { cart.length > 0 ? <div className={style.cartNumber}>{cart.length}</div> : ''}
+                    {/* Div with numbers will be displayed based on cart length, if 0 it won't be displayed at all */}
+                    { cart.length > 0 ? <div className={`${style.cartNumber} ${onCartUpdate ? style.cartUpdate : ''}`}>{cart.length}</div> : ''}
                     <img className={style.img} src="./assets/icons/cart-icon.png"/>
             </NavLink>
         </nav>
