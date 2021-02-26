e86231a5c1060245fd4e8f7190600d05ce3fed0a
Lisa <lisastina.hansson@gmail.com>
2021-02-26 15:13:29 +0100

    Added Routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import Checkout from './pages/Checkout'
+import Confirmation from './pages/Confirmation'
+import Details from './pages/Details'
+
+
 function App() {
-  return <div className="App">
-    <h1>Hello world</h1>
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/checkout" component={Checkout}/>
+        <Route exact path="/confirmation" component={Confirmation}/>
+        <Route exact path="/details" component={Details}/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
+ 
