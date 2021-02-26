b624714d651ec09f96f2cea93df9d69290ec752c
Chris <to.chris.lindstrom@gmail.com>
2021-02-26 15:27:20 +0100

    Added links to logo and checkout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,10 @@ import style from '../css/Navbar.module.css';
 const Navbar = () => {
     return ( 
         <nav className={style.navbar}>
+            <NavLink exact to="/">A LOGO</NavLink>
             <NavLink exact to="/">Home</NavLink>
             <NavLink exact to="/about">About</NavLink>
+            <NavLink exact to="/checkout">CART</NavLink>
         </nav>
      );
 }
