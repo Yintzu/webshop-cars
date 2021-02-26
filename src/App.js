a0ad06a1e64c45f57542e043da6288f628bb5c58
Victor Werngren <victorlillowerngren@gmail.com>
2021-02-26 15:10:15 +0100

    Started Checkout layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import Checkout from "./pages/Checkout";
+
 function App() {
   return <div className="App">
+    <Checkout/>
     <h1>Hello world</h1>
   </div>;
 }
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,8 +2,18 @@ import style from '../css/Checkout.module.css';
 
 const Checkout = () => {
     return ( 
-        <div>
+        <div className="checkout">
+            <div className="container text-center">
+                <h1>Your shopping cart</h1>
+                <div className={style.shoppinglist}>
+                    <p>Item</p>
+                    <p>Item</p>
+                    <p>Item</p>
+                    <p>Item</p>
+                    <p>Item</p>
+                </div>
 
+            </div>
         </div>
      );
 }
