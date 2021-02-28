11a1b9c94336a846b8b13655c2f4584d3301cd64
Victor Werngren <victorlillowerngren@gmail.com>
2021-02-28 11:02:28 +0100

    Added code to display sum total on Checkout Page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,7 +4,9 @@ import style from '../css/Checkout.module.css';
 
 const Checkout = () => {
 
-    const { shoppingCartItems, removeFromCart } = useContext(ShoppingCartContext)
+    const { shoppingCartItems, removeFromCart, cartTotal } = useContext(ShoppingCartContext)
+
+    let itemS = shoppingCartItems.length === 1 ? 'item' : 'items';
 
     return (
         <div className="checkout">
@@ -12,20 +14,33 @@ const Checkout = () => {
                 <h1>Your shopping cart</h1>
                 <div className="row">
                     <div className={style.shoppinglist}>
+                        {/* Ternary operator to display "No items in cart" or loop out the items */}
                         {shoppingCartItems.length == 0 ? <h2 className="text-center">No items in cart</h2> :
-                            shoppingCartItems.map((item, key) => (
-                                <div key={key} className={`row ${style.shoppingCartCard}`}>
-                                    <div className="col-2">Image Placeholder</div>
-                                    <div className="col-7">
-                                        <h2>{`${item.make} ${item.model}`}</h2>
-                                        <p>{`${item.descShort}`}</p>
+                            <div>
+                                {shoppingCartItems.map((item, key) => (
+                                    <div key={key} className={`row ${style.shoppingCartCard}`}>
+                                        <div className="col-2">Image Placeholder</div>
+                                        <div className="col-7">
+                                            <h2>{`${item.make} ${item.model}`}</h2>
+                                            <p>{`${item.descShort}`}</p>
+                                        </div>
+                                        <div className={`col-2 ${style.flexer}`}>
+                                            <p className="mb-0"><strong>{`${new Intl.NumberFormat('sv-SE', { currency: 'SEK', style: 'decimal' }).format(item.price)} kr`}</strong></p>
+                                        </div>
+                                        <div className={`col-1 ${style.flexer}`}><span className={style.removeButton} onClick={() => removeFromCart(item)}>X</span></div>
                                     </div>
-                                    <div className={`col-2 ${style.flexer}`}>
-                                        <p className="mb-0"><strong>{`${item.price} kr`}</strong></p>
-                                    </div>
-                                    <div className={`col-1 ${style.flexer}`}><span className={style.removeButton} onClick={() => removeFromCart(item)}>X</span></div>
+                                ))}
+                                <hr />
+                                <div className="d-flex justify-content-between">
+                                    <h3>
+                                        {`${shoppingCartItems.length} ${itemS} in cart`}
+                                    </h3>
+                                    <h3>
+                                        {`${new Intl.NumberFormat('sv-SE', { currency: 'SEK', style: 'decimal' }).format(cartTotal)} kr`}
+                                    </h3>
                                 </div>
-                            ))}
+                            </div>
+                        } {/* Ternary operator end */}
                     </div>
                 </div>
 
