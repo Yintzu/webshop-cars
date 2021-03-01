5274564a41a921c6e811e49fd94a8d55355f0288
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-01 14:50:41 +0100

    Change of title and some content Confirmation page

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,10 +1,34 @@
-import style from '../css/Confirmation.module.css';
+import styles from '../css/Confirmation.module.css';
+import Navbar from '../components/Navbar';
 
 const Confirmation = () => {
     return ( 
-        <div>
-            
+        <div className="container">
+          <h1>Tack för ditt köp</h1>
+            <div className="row"> 
+              <div className={`
+              ${styles.confirmation}
+              col-sm
+              col-md-offset-2
+              `}>                 
+                <h3>Order number: 1275758489</h3>
+                  <p>Delivery details</p>
+                  
+              </div>
+              <div className={`
+                ${styles.confirmation}
+                mx-md-2
+                col-sm
+                col-md-offset-5
+              `}>  
+                <h3>Summary</h3>
+                <p>Totalbelopp</p>
+              </div>
+                
+                
+            </div>
         </div>
+  
      );
 }
  
