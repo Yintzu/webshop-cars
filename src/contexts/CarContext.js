b330c5832d7c077871402db819abda0c8d9f67d1
Denis <danielimamovic@gmail.com>
2021-03-01 10:13:28 +0100

    Grundkod för Carcontext.js
    Co-authored-by: Christopher Lindström <to.chris.lindstrom.gmail.com>

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,7 +1,16 @@
-const CarContextProvider = () => {
-  return ( 
+import {
+    createContext
+} from "react";
+export const CarContext = createContext()
+
+const CarContextProvider = (props) => {
     
-   );
+    return (
+        <CarContext.Provider>
+            {props.children}
+
+        </CarContext.Provider>
+    );
 }
- 
+
 export default CarContextProvider;
