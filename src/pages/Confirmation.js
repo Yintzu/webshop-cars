3997093ab5e27ecfbd974101420cbbae7c80300b
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-01 19:24:38 +0100

    Changed some styling to confirmation page

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -11,43 +11,52 @@ const Confirmation = () => {
               col-md-offset-2
               `}>                 
                 <h3>Order number: 1275758489</h3>
-                  <p>Delivery details</p>
+                  <p className={styles.containerText}>Delivery details</p>
+                    <hr/>
+                    <h4>Delivery for</h4>
+                    <p className={styles.containerText}>Mrs Susanne Eneroth</p>
+                    <h4>Delivery address</h4>
+                    <p className={styles.containerText}>Storgatan 6</p>
+                    <p className={styles.containerText}>217 57 Malmö</p>
+                    <h4>Telephone</h4>  
+                    <p className={styles.containerTextLast}>+46707171929</p>
                     <button type="button" 
                     className={`
                     ${styles.backButton}
                     btn 
                     btn-dark
                    `}>Back</button>
-
                     <button type="button" className="btn btn-info">Print</button>
               </div>
+
+              {/* Start box right - Summary */}
               <div className={`
                 ${styles.confirmationBox}
                 mx-md-2
                 col
                 col-lg-4
                 col-md-offset-5
-              `}>  
+              `}> 
                 <h3>Summary</h3>
                 <hr/>
                 <div className="row">
                   <div className="col">
-                    <p>Car 1</p>
-                    <p>Car 2</p>
+                    <p className={styles.containerText}>Car 1</p>
+                    <p className={styles.containerText}>Car 2</p>
                     <hr/>
                  </div>
                  <div className="col">
-                    <p>$200 000</p>
-                    <p>$450 000</p>
+                    <p className={styles.containerText}>$200 000</p>
+                    <p className={styles.containerText}>$450 000</p>
                     <hr/> 
                  </div>
                 </div>
                 <div className="row">
                   <div className="col">
-                    <p>Total:</p>
+                    <p className={styles.containerText}>Total:</p>
                  </div>
                  <div className="col">
-                    <p>$650 000</p>
+                    <p className={styles.containerText}>$650 000</p>
                  </div>
                 </div>
                
