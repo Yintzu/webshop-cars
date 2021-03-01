bb0f60a865309bca97a975ab7d3cee7da8333b75
Denis <danielimamovic@gmail.com>
2021-03-01 10:30:37 +0100

    Kopplat carContext med app.js och lagt in useState.
    Co-authored-by: Christopher Lindström <to.chris.lindstrom@gmail.com>

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,9 +1,11 @@
 import {
-    createContext
+    createContext,
+    useState
 } from "react";
 export const CarContext = createContext()
 
 const CarContextProvider = (props) => {
+    const [cars, setcars]= useState([])
     
     return (
         <CarContext.Provider>
