479f31e3f23793cdc027756f664d602e33fe78b1
Chris <to.chris.lindstrom@gmail.com>
2021-03-01 13:45:30 +0100

    Added basic layout to each car item

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,10 +1,21 @@
 import { useContext } from 'react';
+import { CarContext } from '../contexts/CarContext';
 import style from '../css/TestPage.module.css';
 
 const TestPage = () => {
+  const { cars } = useContext(CarContext);
+
   return ( 
     <div className={style.testPageWrapper}>
-      
+      {cars.length ? 
+        <div className={style.carCard}>
+          <h5>{cars[0].make}</h5>
+          <h5>{cars[0].model}</h5>
+          <p>{cars[0].year}</p>
+          <button className={style.addToCartBtn}>Add To Cart</button>
+        </div>
+        
+        : <div>Loading...</div>}
     </div>
    );
 }
