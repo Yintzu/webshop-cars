427f9a5705aad5d364e8c729bad8d731948c39b2
Denis <danielimamovic@gmail.com>
2021-03-02 21:37:29 +0100

    Made CarCard.js code more dynamic
    More dynamic using variables from carcontext on carCard texts like Model,titel,age etc

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,40 +1,27 @@
 import style from '../css/CarCard.module.css';
+import {useContext } from "react";
+import {CarContext } from "../contexts/CarContext";
 
 const CarCard = () => {
+    const {cars} = useContext(CarContext); 
+    console.log(cars);   
+    let carorder;
     return (
         <div className="row">
             <div class="col-sm-6">
                 <div class="card mt-2 mx-2">
-                    <img src="assets/car-pictures/Volvo-XC70-2006.jpg" className="card-img-top" alt="A good affordable car" />
+                    <img src={cars[0].carImg} className="card-img-top" alt="A good affordable car" />
                     <div className="card-body">
-                        <h5 className="card-title">Bilnamn och modell</h5>
-                        <p className="card-text">Shortdesc</p>
-                        <a href="#" className="btn btn-primary float-end">Buy</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div className="card mt-2 mx-2">
-                    <img src="assets/car-pictures/Volvo-XC70-2006.jpg" className="card-img-top" alt="A good affordable car" />
-                    <div className="card-body">
-                        <h5 className="card-title">Bilnamn och modell</h5>
-                        <p className="card-text">Shortdesc</p>
-                        <a href="#" className="btn btn-primary float-end">Buy</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div className="card mt-2 mx-2">
-                    <img src="assets/car-pictures/Volvo-XC70-2006.jpg" className="card-img-top" alt="A good affordable car" />
-                    <div className="card-body">
-                        <h5 className="card-title">Bilnamn och modell</h5>
-                        <p className="card-text">Shortdesc</p>
+                        <h5 className="card-title">{cars[0].make} {cars[0].model} {cars[0].year}</h5>
+                        <p className="card-text">{cars[0].descShort}</p>
                         <a href="#" className="btn btn-primary float-end">Buy</a>
                     </div>
                 </div>
             </div>
         </div>
+        
     );
+    
 }
 
 export default CarCard;
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,9 +1,16 @@
 import style from '../css/CarList.module.css';
+import { useContext, useEffect, useState } from 'react';
+import {CarContext } from "../contexts/CarContext";
+import CarCard from './CarCard';
+
 
 const CarList = () => {
     return ( 
         <div>
-            
+            <CarCard>
+
+
+            </CarCard>
         </div>
      );
 }
