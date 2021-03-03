0dc84cd211d9c86b00a02f0536a75b3fd8176c5a
Chris <to.chris.lindstrom@gmail.com>
2021-03-03 13:53:32 +0100

    Started adding basic layout to popup-cart

--- src/components/PopupCart.js
+++ src/components/PopupCart.js
@@ -4,10 +4,25 @@ import style from '../css/PopupCart.module.css';
 
 const PopupCart = () => {
   const { shoppingCartItems } = useContext(ShoppingCartContext);
-  
+
   return ( 
     <div className={style.popupCartWrapper}>
-
+      <div className={style.cartItemsWrapper}>
+        {shoppingCartItems.map(car => (
+          <div className={style.cartItem} key={car.vin}>
+            <div className={style.cartImgWrapper}>
+              <img src={car.carImg} alt={car.make}/>
+            </div>
+            <div className={style.cartItemText}>
+              <h5 className={style.cartCarName}>
+                {car.make} 
+                {car.model} 
+              </h5>
+              <p className={style.cartYear}>{car.year}</p>
+              </div>
+          </div>
+        ))}
+      </div>
     </div>
    );
 }
