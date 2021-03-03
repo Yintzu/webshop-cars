3eaf3dece80b022ce514c4e22acaf5722fb5708e
Lisa <lisastina.hansson@gmail.com>
2021-03-03 15:16:02 +0100

    Linked details-page to carCard image

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,13 +1,18 @@
 import { ShoppingCartContext } from '../contexts/ShoppingCartContext';
+import { CarContext } from "../contexts/CarContext";
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import style from '../css/CarCard.module.css';
 
 const CarCard = (props) => {
+    const history = useHistory();
     const { addToCart, removeFromCart,shoppingCartItems } = useContext(ShoppingCartContext);
+    const { viewCar } = useContext(CarContext);
 
     return (
-        <div className="col-sm-6" >
+        <div className={`col-sm-6 ${style.carCard}`}>
             <div className="card mt-2 mx-2">
-                <img src={props.car.carImg} className="card-img-top" alt="A good affordable car" />
+                <img src={props.car.carImg} className="card-img-top" alt="A good affordable car" onClick={() => viewCar(props.car, history)}/>
                 <div className="card-body">
                     <h5 className="card-title">{props.car.make} {props.car.model} {props.car.year}</h5>
                     <p className="card-text">{props.car.descShort}</p>
