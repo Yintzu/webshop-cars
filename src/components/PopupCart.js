0922c121641cc5afe9dd42cbf09ba4b3d46ab251
Chris <to.chris.lindstrom@gmail.com>
2021-03-03 11:36:46 +0100

    Created popup-cart component

---
+++ src/components/PopupCart.js
@@ -0,0 +1,11 @@
+import style from '../css/PopupCart.module.css';
+
+const PopupCart = () => {
+  return ( 
+    <div className={style.popupCartWrapper}>
+
+    </div>
+   );
+}
+ 
+export default PopupCart;
