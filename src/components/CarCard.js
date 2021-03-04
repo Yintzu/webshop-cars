8fc7fb8826bbc98275985a012e686673e0f504f0
Denis <Danielarbete@protonmail.com>
2021-03-04 09:08:14 +0100

    Added price text on CarCard

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -10,6 +10,7 @@ const CarCard = (props) => {
                 <img src={props.car.carImg} className="card-img-top" alt="A good affordable car" />
                 <div className="card-body">
                     <h5 className="card-title">{props.car.make} {props.car.model} {props.car.year}</h5>
+                    <p className="card-text">{props.car.price}kr</p>
                     <p className="card-text">{props.car.descShort}</p>
                     <button onClick={() => addToCart(props.car) }className="btn btn-primary float-end">Add To Cart</button>
                 </div>
