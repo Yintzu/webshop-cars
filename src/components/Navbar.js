aeb549a694a1da39f7063ff4378d14fa80fc9cba
Chris <to.chris.lindstrom@gmail.com>
2021-03-09 10:45:24 +0100

    Testing cart icon and number positions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ const Navbar = () => {
                     {/* <NavLink className={style.links} activeClassName={style.active} exact to="/testpage">Support</NavLink> */}
                 </div>
                 <div className={style.iconsWrapper}>
-                    <div onMouseOver={mouseOverHandler} onMouseLeave={mouseLeaveHandler} className={style.cartIconWrapper} onClick={cartClickHandler}>
+                    <div onMouseOver={mouseOverHandler} onMouseLeave={mouseLeaveHandler} className={`${style.cartIconWrapper} ${cart.length && style.cartMoveDown}`} onClick={cartClickHandler}>
                         {/* Div with numbers will be displayed based on cart length, if 0 it won't be displayed at all */}
                         {cart.length > 0 && <div className={`${style.cartNumber} ${onCartUpdate && style.cartUpdate}`}><span>{cart.length}</span></div>}
                         <img className={style.img} src="/assets/icons/shopping-cart-web.png" />
@@ -116,9 +116,9 @@ const Navbar = () => {
                             <PopupCart />
                             <div className={style.cartShadow} />
                         </div>}
-                    {/* <NavLink className={style.acctContact} exact to="/">
+                    <NavLink className={style.acctContact} exact to="/">
                         <img className={style.acctContactImg} src="/assets/icons/account-contact-circle.png" />
-                    </NavLink> */}
+                    </NavLink>
                 </div>
             </nav>
             <aside className={style.infoBar}>
