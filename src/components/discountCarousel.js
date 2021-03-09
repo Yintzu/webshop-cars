741edee93a0bdb4e65f7cfaf40084b34b673ad1b
Denis <Danielarbete@protonmail.com>
2021-03-09 10:42:12 +0100

    Rounded corners of Carousel

--- src/components/discountCarousel.js
+++ src/components/discountCarousel.js
@@ -13,7 +13,7 @@ const Carousel = (props) => {
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div className="carousel-inner">
+  <div className="carousel-inner rounded-3">
     <div className="carousel-item active">
       <img src="../assets/car-pictures/Chevrolet-Camaro-1973.jpg" className="d-block w-100" alt="Discounted Carousel"/>
       <div className="carousel-caption d-none d-md-block">
