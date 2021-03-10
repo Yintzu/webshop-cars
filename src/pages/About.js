6ffc74116f601ffe8f7279b7693833e8750c6c15
Tricia <tricia.hartmann@medieinstitutet.se>
2021-03-10 14:15:33 +0100

    Added text to paragraphs in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,13 @@ const title ='About us';
     return ( 
        <div className="About">
         <h1> { title }</h1>
+        <p>Quality car retailer of Sweden
+
+            With an extensive range of approved used cars featuring all makes and models, Richard Ryan's Reliable Rides can provide the right car at the right price. With so much choice why go anywhere else? Get the best prices and widest choice on quality used cars in Sweden, contact us today!</p>
+
+            <p>We have quality used cars in stock. We can also deliver cars anywhere in Sweden.</p>
+            
+            <p>Find your dream car today!</p>
       </div>
      );
 }
