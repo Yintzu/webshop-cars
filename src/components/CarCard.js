f72ba4f1c2f7a5523c62cee12fec2ebd06ed1a84
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-11 21:26:13 +0100

    New card css for desktop

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -27,7 +27,7 @@ const CarCard = (props) => {
         } else if (bought){
           return <button className={`btn btn-secondary ${style.disabled} ${style.btnCustom}`} id="addRemove">Sold</button>
         } else {
-          return <button onClick={() => addToCart(car)} className={`btn btn-primary ${style.btnCustom}`} id="addRemove">Add To Cart</button> 
+          return <button onClick={() => addToCart(car)} className={`btn btn-primary ${style.addButton}`} id="addRemove">Add To Cart</button> 
         }
       }
 
@@ -39,16 +39,18 @@ const CarCard = (props) => {
                   <img src={props.car.carImg} className={`${style.carImg}`}alt="A good affordable car" />
                 </div>
                 <div className={style.infoRow}>
-                  <h5 className={`${style.cardTitle}`}>{props.car.make} {props.car.model} {props.car.year}</h5>
+                  <h5 className={`${style.cardTitle}`}>{props.car.make} {props.car.model} </h5>
+                  <p className={style.carDetailText}><span className={style.boldText}>Price:</span> {formatSum(props.car.price)} <span className={style.boldText}>Year:</span> {props.car.year}</p>
+                  <hr className={style.hrCard}/>
                   <p className={`${style.cardDesc}`}>{props.car.descShort}</p>
                 </div>
             </div>
             <div className={style.buyRow}>
             
               <div className={style.priceWrapper}>
-                <h5 className={`${style.cardPrice}`}>{formatSum(props.car.price)}</h5>
+                {/* <h5 className={`${style.cardPrice}`}>{formatSum(props.car.price)} </h5> */}
               </div>
-              <button className={`btn btn-secondary ${style.btnCustom} ${style.btnReadMore}`}>Read more</button>
+              {/* <button className={`btn btn-secondary ${style.readMore} ${style.btnReadMore}`}>Read more</button> */}
               { renderButtons(props.car) }
             </div>
         </div>
