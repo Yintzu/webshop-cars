4eb8b2669ad151fae34d9c4da2b77d0a1ae28cb4
Tricia <tricia.hartmann@medieinstitutet.se>
2021-03-11 20:03:02 +0100

    editing css code
    added code to about.module.css , and fixing issues with alignment about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,43 +6,66 @@ const About = () => {
   const link2 = 'How we deliver';
   const link3 = 'Environmental Impact?';
   const title = 'About us';
+  const title1 = 'Who are we?';
+  const title2 = 'How we deliver';
+  const title3 = 'Environmental Impact';
 
     return ( 
-    <div>
-      <div className="menu-container">
-
-        <div className="link1">
-          <p>{ link1 }</p>
+   
+      <div className="container">
+        
+        <div className="menu">
+          <div className="row">
+            <a href="/">{link1}</a>
+          
+            <a href="/">{link2}</a>
+          
+            <a href="/">{link3}</a>
+          </div>
         </div>
-  
-        <div className="link2">
-          <p>{ link2 }</p>
-        </div>
-
-        <div className="link3">
-           <p>{ link3 }</p>
-        </div>
-      </div>
- 
-      <div className="content">
-
+        
         <div className="page-title">
           <h1>{ title }</h1>
-      
+        </div>
+
+        <div className="text">
           <p>Quality car retailer of Sweden</p>
         
 
           <p>With an extensive range of approved used cars featuring all makes and models, Richard Ryan's Reliable Rides can provide the right car at the right price. With so much choice why go anywhere else? Get the best prices and widest choice on quality used cars in Sweden, contact us today!</p>
+        </div>
 
-          <p>Who are we?</p>
-          <p>How we deliver</p>
-          <p>Our environmental impact.</p>
-              
-          <p>Find your dream car today!</p>
+          <div className="page-title">
+            <h1>{ title1 }</h1>
+          </div>
+
+          <div className="text">
+            <p>Nulla facilisi. Aenean rutrum tortor nec sem commodo faucibus. Vestibulum nunc erat, vestibulum vitae eros et, mollis placerat purus. Quisque consectetur pellentesque metus, non dictum elit placerat vel. Aenean iaculis tincidunt orci, in scelerisque velit porta vitae. Vivamus et malesuada mauris. Proin ligula magna, lacinia quis euismod in, vehicula nec turpis. Aenean tempus fermentum felis nec interdum. Praesent eu accumsan ex, sed pulvinar urna. Mauris sit amet lectus a nulla egestas feugiat eu nec justo. </p>
+          </div>
+
+          <div className="page-title">
+            <h1>{ title2 }</h1>
+          </div>
+
+          <div className="text">
+            <p>Proin nec urna sed tellus fringilla mattis. Aenean scelerisque tortor ut felis imperdiet elementum. Nunc a euismod dolor. Aliquam gravida erat nec feugiat vulputate. Donec in nunc sollicitudin, tempor nibh eu, ultricies magna. Mauris massa justo, hendrerit a accumsan a, convallis eleifend felis. Praesent elit ipsum, lacinia et justo nec, tincidunt maximus risus. Duis ligula nibh, eleifend ornare iaculis nec, pretium et nunc. In facilisis, nulla sed ullamcorper tempor, orci augue interdum sapien, vel fringilla sem turpis vitae lacus. Sed eget iaculis mi, vitae suscipit lorem. </p>
+          </div>
+
+          <div className="page-title">
+            <h1>{ title3 }</h1>
+          </div>
+
+          <div className="text">
+            <p>Nullam pulvinar tellus rhoncus sapien dignissim, quis mattis dui vestibulum. Pellentesque auctor massa efficitur est tincidunt aliquam. In tincidunt leo non enim faucibus lacinia. Maecenas suscipit malesuada consectetur. Pellentesque posuere, sem in volutpat vulputate, metus purus venenatis sem, ac imperdiet turpis nibh in diam. Phasellus eget consequat nunc, quis ornare sem. Aenean quis velit sed sapien tempus auctor. Nunc ac libero risus. Vivamus condimentum lectus in est semper, id suscipit nunc mollis. </p>
+
+            <p>Sed non suscipit tellus, non viverra dolor. Vivamus varius mi convallis, ornare ex vel, egestas ligula. Pellentesque ac risus at arcu feugiat luctus a a ligula. Nunc commodo pulvinar ante id pulvinar. Nulla venenatis aliquam neque. Vestibulum eu erat varius, dignissim magna porta, iaculis augue. Sed aliquet sit amet sem id aliquet. Donec sed suscipit velit. Pellentesque vel varius dui, quis suscipit velit. Mauris scelerisque, velit et feugiat rhoncus, urna sapien convallis ante, sit amet imperdiet tellus metus ac enim. Nunc sed facilisis mi. 
+          </p>
+            
+            <p>Find your dream car today!</p>
 
         </div>
       </div>
-    </div>
+  
    );
   }
 
