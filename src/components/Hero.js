d3a3a210623004b227090ec6c5c63e21cd482aa1
Chris <to.chris.lindstrom@gmail.com>
2021-03-11 17:53:04 +0100

    Small text change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className={`${style.heroCenterDiv} ${style.flexCenter}`}>
                 <div className={`row ${style.flexCenter}`}>
                     <div className="col-12 col-md-4">
-                        <h1 className={`${style.h1}`}>Welcome to the most realiable car shop</h1>
+                        <h1 className={`${style.h1}`}>Welcome to the most reliable car shop</h1>
                         <p className={`${style.p}`}>A car is special. It reflects who you are. Here on Richard Ryan's Reliable Rides we offer you realiable vessels that will last many years and accompany you through life's many adventures.</p>
                     </div>
                     <div className="col-12 col-md-7">
