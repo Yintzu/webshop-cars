79152bf8a28c903adaa918e8128d6fe80430f5f2
Denis <Danielarbete@protonmail.com>
2021-03-15 13:16:48 +0100

    fixed Home.js and Hero.js failure to import discountCarousel component
    :(

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import style from '../css/Home.module.css';
 import CarList from '../components/CarList';
-import Carousel from '../components/DiscountCarousel';
+import Carousel from '../components/discountCarousel';
 import Search from '../components/Search';
 
 const Home = () => {
