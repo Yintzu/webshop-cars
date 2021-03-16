ac8581cc7bc9f56015af8eb2d6e1be1e8c2ef372
Victor Werngren <victorlillowerngren@gmail.com>
2021-03-16 14:29:04 +0100

    Created component and hooked up the route
    Co-authored-by: Tricia <tricia.hartmann@medieinstitutet.se>

---
+++ src/pages/CreateAccount.js
@@ -0,0 +1,19 @@
+const CreateAccount = () => {
+    return ( 
+        <div>
+            <h1>Create new account</h1>
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" id="registerName"></input>
+                    <input type="text" id="registerUserName"></input>
+                    <input type="text" id="registerPhone"></input>
+                </div>
+                <div className="col-6">
+
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default CreateAccount;
