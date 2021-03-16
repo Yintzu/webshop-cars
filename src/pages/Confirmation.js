fab0b44925813ef4d068b0ab8c161bef8ca3a104
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-16 16:32:51 +0100

    Small dev change

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -17,7 +17,7 @@ const Confirmation = () => {
 
   return (
     <div className="container">
-    <ProfileInfo /> <ProfileCar />
+    <ProfileInfo />
       <h1 className={styles.mainHeading}>Thank you for your order</h1>
       <div className="row">
         <div className={`
