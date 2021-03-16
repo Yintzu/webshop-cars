5668fedf49c7721925fdca586c1136ce0a335ceb
Victor Werngren <victorlillowerngren@gmail.com>
2021-03-16 14:57:03 +0100

    Fixed input fields

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,19 +1,41 @@
+import style from "../css/CreateAccount.module.css"
+
 const CreateAccount = () => {
-    return ( 
+    return (
         <div>
-            <h1>Create new account</h1>
+            <h1 className={style.h1}>Create new account</h1>
             <div className="row">
                 <div className="col-6">
-                    <input type="text" id="registerName"></input>
-                    <input type="text" id="registerUserName"></input>
-                    <input type="text" id="registerPhone"></input>
+                    <label htmlFor="registerFullName">Full name</label>
+                    <input className={`form-control`} type="text" id="registerFullName"></input>
+
+                    <label htmlFor="registerPersonalNumber">Personal number</label>
+                    <input className={`form-control`} type="text" id="registerPersonalNumber"></input>
+
+                    <label htmlFor="registerUserName">User name</label>
+                    <input className={`form-control`} type="text" id="registerUserName"></input>
+
+                    <label htmlFor="registerPhone">Phone number</label>
+                    <input className={`form-control`} type="text" id="registerPhone"></input>
                 </div>
                 <div className="col-6">
+                    <label htmlFor="registerEmail">E-mail address</label>
+                    <input className={`form-control`} type="text" id="registerEmail"></input>
 
+                    <label htmlFor="registerConfirmEmail">Confirm e-mail address</label>
+                    <input className={`form-control`} type="text" id="registerConfirmEmail"></input>
+
+                    <label htmlFor="registerPassword">Password</label>
+                    <input className={`form-control`} type="text" id="registerPassword"></input>
+
+                    <label htmlFor="registerConfirmPassword">Confirm password</label>
+                    <input className={`form-control`} type="text" id="registerConfirmPassword"></input>
                 </div>
             </div>
+
+            <button className={`btn ${style.registerButton}`}>Register</button>
         </div>
-     );
+    );
 }
- 
+
 export default CreateAccount;
