4ca75815e550ee1c62c1a32cc3516885749791ce
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-18 16:50:08 +0100

    Added grid to card on profile page

--- src/components/ProfileCar.js
+++ src/components/ProfileCar.js
@@ -6,19 +6,16 @@ const ProfileCar = () => {
         <div>
            <div className={`
            ${styles.containerWrapper}
-           container
            `}>
            <h3 className={styles.profileCarHeading}>My purchase history</h3>
-           <div className={styles.profileCarWrapper}>
-           <img src={labrador_profile} alt="Profile picture"   className={styles.profileImage}/>
-             <h4 className={styles.profileCarSubHeading}>Order number</h4>
-             <h4 className={styles.profileCarSubHeading}>Car make</h4>
-             <div className={styles.profileTextBox}>
-             <p className={styles.profileText}>Time and date of purchase</p>
-             <p className={styles.profileText}>Description Lorem,   ipsum dolor sit amet consectetur adipisicing elit.</p>
-             </div>
-             
-             </div>
+           <div className={`${styles.profileCarWrapper}`}>
+           <img src={labrador_profile} alt="Profile picture" className={`${styles.profileImage} ${styles.gridItem1}`}/>
+             <h4 className={`${styles.profileCarSubHeading} ${styles.gridItem2}`}>Order number</h4>
+             <h4 className={`${styles.profileCarSubHeading} ${styles.gridItem3}`}>Car make</h4>
+             <p className={`${styles.profileText} ${styles.gridItem4}`}>Time</p>
+             <p className={`${styles.profileText} ${styles.gridItem5}`}>Detail button?</p>
+           
+         </div>
          </div>
         </div>
      );
