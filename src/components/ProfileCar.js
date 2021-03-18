67bff84e9ecaf95f67edf725fad7a841be4ba1ca
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-18 17:06:44 +0100

    Added placeholder image and change of colors

--- src/components/ProfileCar.js
+++ src/components/ProfileCar.js
@@ -9,11 +9,11 @@ const ProfileCar = () => {
            `}>
            <h3 className={styles.profileCarHeading}>My purchase history</h3>
            <div className={`${styles.profileCarWrapper}`}>
-           <img src={labrador_profile} alt="Profile picture" className={`${styles.profileImage} ${styles.gridItem1}`}/>
-             <h4 className={`${styles.profileCarSubHeading} ${styles.gridItem2}`}>Order number</h4>
-             <h4 className={`${styles.profileCarSubHeading} ${styles.gridItem3}`}>Car make</h4>
-             <p className={`${styles.profileText} ${styles.gridItem4}`}>Time</p>
-             <p className={`${styles.profileText} ${styles.gridItem5}`}>Detail button?</p>
+           <img src="https://via.placeholder.com/100x50.png" alt="Profile picture" className={`${styles.profileImage} ${styles.gridItem1}`}/>
+             <p className={`${styles.profileText} ${styles.gridItem2}`}>Ordernumber</p>
+             <p className={`${styles.profileText} ${styles.gridItem3}`}>Time</p>
+             <p className={`${styles.profileText} ${styles.gridItem4}`}>Car make</p>
+             <p className={`${styles.profileText} ${styles.gridItem5}`}>Price</p>
            
          </div>
          </div>
