4a564dccb1157783d000626f93526a810d5fbd93
Su Eneroth <susanne.eneroth@doggson.se>
2021-03-19 11:24:36 +0100

    Profile car confirmation card
    Co-authored-by: Lisa Hansson <lisastina.hansson@gmail.com>

--- src/components/ProfileCar.js
+++ src/components/ProfileCar.js
@@ -3,20 +3,31 @@ import labrador_profile from './images/labrador_profile.jpg'
 
 const ProfileCar = () => {
     return ( 
-        <div>
+        <div className={`
+        ${styles.wrapper}
+        `}>
+           <p className={`${styles.profileCarSubHeading}`}>Ordernumber</p>
+             <p className={`${styles.profileCarSubheading}`}>Date/Time</p>
            <div className={`
-           ${styles.containerWrapper}
+           ${styles.containerCarWrapper}
            `}>
-           <h3 className={styles.profileCarHeading}>My purchase history</h3>
            <div className={`${styles.profileCarWrapper}`}>
            <img src="https://via.placeholder.com/100x50.png" alt="Profile picture" className={`${styles.profileImage} ${styles.gridItem1}`}/>
-             <p className={`${styles.profileText} ${styles.gridItem2}`}>Ordernumber</p>
-             <p className={`${styles.profileText} ${styles.gridItem3}`}>Time</p>
-             <p className={`${styles.profileText} ${styles.gridItem4}`}>Car make</p>
-             <p className={`${styles.profileText} ${styles.gridItem5}`}>Price</p>
+          
+             <p className={`${styles.profileText} ${styles.gridItem2}`}>Car make</p>
+             <p className={`${styles.profileText} ${styles.gridItem3}`}>Price</p>
+             <p className={`${styles.profileText} ${styles.gridItem4}`}>Year</p>
+             {/* <p className={`${styles.profileText} ${styles.gridItem5}`}>Car make</p> */}
            
          </div>
          </div>
+         <p className={`${styles.profileCarSubheading}`}>Total price</p>
+         <button type="button"
+            className={`
+                     ${styles.infoButton}
+                     btn 
+                     btn-info`}
+          >Print</button>
         </div>
      );
 }
