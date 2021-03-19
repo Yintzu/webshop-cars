00f6a9d90d7143d3a2b1435c3dfa972cb565c027
Chris <to.chris.lindstrom@gmail.com>
2021-03-19 11:38:31 +0100

    Creating modal component for details page
    Co-authored-by: Tricia <tricia.hartmann@medieinstitutet.se>

---
+++ src/components/ModalTest.js
@@ -0,0 +1,11 @@
+import style from '../css/ModalTest.module.css';
+
+const ModalTest = () => {
+  return ( 
+    <div className={style.detailsModalWrapper}>
+ 
+    </div>
+   );
+}
+ 
+export default ModalTest;
