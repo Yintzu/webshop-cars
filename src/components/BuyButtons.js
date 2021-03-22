fe93e1b07cfdebd249be2ef980a91a30e351f8fa
Chris <to.chris.lindstrom@gmail.com>
2021-03-22 14:27:05 +0100

    Added button component to home and to details
    Adjusted CSS on btn and on each page

--- src/components/BuyButtons.js
+++ src/components/BuyButtons.js
@@ -1,10 +1,35 @@
 import { useContext } from 'react';
+import { CarContext } from '../contexts/CarContext';
+import { ShoppingCartContext } from '../contexts/ShoppingCartContext';
 import style from '../css/BuyButtons.module.css';
 
-const BuyButtons = () => {
+const BuyButtons = (props) => {
+  const { boughtCheck } = useContext(CarContext);
+  const { shoppingCartItems, removeFromCart, addToCart } = useContext(ShoppingCartContext);
+
+  const renderButtons = (car) => {
+    let inCart = false;
+    let bought = false;
+    shoppingCartItems.forEach(cartItem => {
+      if (cartItem.vin === car.vin) {
+        inCart = true;
+      }
+    });
+
+    bought = boughtCheck(car);
+
+    if (inCart) {
+      return <button onClick={() => removeFromCart(car)} className={`${style.btnsWidth} ${style.removeButton}`} id="addRemove">Remove</button>
+    } else if (bought) {
+      return <button className={`${style.btnsWidth} ${style.disabled} ${style.btnCustom}`} id="addRemove">Sold</button>
+    } else {
+      return <button onClick={() => addToCart(car)} className={`${style.btnsWidth} ${style.addButton}`} id="addRemove">Buy</button>
+    }
+  }
+
   return ( 
     <div className={style.buttonsWrapper}>
-      
+      {renderButtons(props.car)}
     </div>
    );
 }
