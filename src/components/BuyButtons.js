e4a01046f3062ad55dea3eb59ff1097b89d64f1c
Chris <to.chris.lindstrom@gmail.com>
2021-03-22 11:49:46 +0100

    Created component file and css file for buttons

---
+++ src/components/BuyButtons.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import style from '../css/BuyButtons.module.css';
+
+const BuyButtons = () => {
+  return ( 
+    <div className={style.buttonsWrapper}>
+      
+    </div>
+   );
+}
+ 
+export default BuyButtons;
