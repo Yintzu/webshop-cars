90c6e6149856aea5a07b88ed1e67ab473368d5fc
Victor Werngren <victorlillowerngren@gmail.com>
2021-03-23 14:49:17 +0100

    Fixes when moving part of carousel to new component

--- src/components/Carouselslide.js
+++ src/components/Carouselslide.js
@@ -13,13 +13,13 @@ const Carouselslide =()=> {
     return (   
         <div>
             {discountedCars.map((discountedcar,index) => ( 
-                <div className={`carousel-item ${index===0&& 'active' ? "active" : ""}`}>
+                <div className={`carousel-item ${index===0&& 'active' ? "active" : ""}`} key={discountedcar.vin}>
                     <div className={style.overlay}></div>
                     {/* <img src={"../assets/carousel/PanozCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/> */}
                     <img src={discountedcar.carImg} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={() => history.push("/details/WAUKF98E25A286122")} />
                     <div className={`${style.carouselcaptioned}  d-md-block ${style.captionWrapper}`}>
-                        <h5>{discountedcar.make} {discountedcar.model} {discountedcar.year}</h5>
-                        <p className="text-danger" >{formatSum(discountedcar.discountedprice())}</p>
+                        <h5 className={`${style.carTitle}`}>{discountedcar.make} {discountedcar.model} {discountedcar.year}</h5>
+                        <p className={`${style.carPrice}`}>{formatSum(discountedcar.discountedprice())}</p>
                     </div>
                 </div>
             ))}
