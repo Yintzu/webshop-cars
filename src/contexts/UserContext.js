4c3d1bd6578ae306b96155a8ef787335fcad3198
Victor Werngren <victorlillowerngren@gmail.com>
2021-03-23 10:43:15 +0100

    Local storage for everything. Needs thorough testing now

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -7,22 +7,44 @@ export const UserContext = createContext();
 const UserContextProvider = (props) => {
   const [isClicked, setIsClicked] = useState(false);
   const { boughtCars, setBoughtCars } = useContext(CarContext)
-  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const [orderInfo, setOrderInfo] = useState([]);
+
   const [users, setUsers] = useState(
     () => {
       const usersLocalData = localStorage.getItem('users');
       return usersLocalData ? JSON.parse(usersLocalData) : []
     }
   );
-  const [orderInfo, setOrderInfo] = useState([]);
+
+  const [loggedInUser, setLoggedInUser] = useState(
+    () => {
+      const loggedInUserLocalData = JSON.parse(localStorage.getItem('loggedInUser'));
+      if (loggedInUserLocalData) {
+        return users.find(user => user.email == loggedInUserLocalData)
+      } else { return null }
+    }
+  );
 
   useEffect(() => {
     localStorage.setItem('users', JSON.stringify(users))
   }, [
     () => {
-      if(loggedInUser.orders){
+      if (loggedInUser.orders) {
         return loggedInUser.orders
       } else return null
+    }
+  ]);
+
+  useEffect(() => {
+    if (loggedInUser) {
+      localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser.email))
+    }
+  }, [
+    () => {
+      if (loggedInUser.email) {
+        return loggedInUser.email
+      } else return loggedInUser
     }
   ]);
 
