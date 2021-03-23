b835ecee7f2fc244812f6f3e52a85a4171b14f16
Denis <Danielarbete@protonmail.com>
2021-03-23 10:19:56 +0100

    final

--- src/components/discountCarousel.js
+++ src/components/discountCarousel.js
@@ -1,13 +1,13 @@
 import { useHistory } from "react-router-dom";
 import { useContext } from "react";
 import style from "../css/DiscountCarousel.module.css";
-import CarContext from "../contexts/CarContext"
-import ShoppingCartContext from "../contexts/ShoppingCartContext"
+import {CarContext} from "../contexts/CarContext"
+import {ShoppingCartContext} from "../contexts/ShoppingCartContext"
 
 const Carousel = () => {
   const history = useHistory();
-  const {discountedCars} = useContext(CarContext)
-  const {formatSum} = useContext(ShoppingCartContext)
+    const {discountedCars}=useContext(CarContext)
+    const {formatSum}=useContext(ShoppingCartContext)
 
 
   // let discountedCars = [
@@ -58,7 +58,7 @@ const Carousel = () => {
       </div>
       <div className={`carousel-inner ${style.carouselWrapper}`}>
         <div className={`carousel-item active`}>
-          {/* <div className={style.overlay}></div> */}
+          <div className={style.overlay}></div>
           {/* <img src={"../assets/carousel/PanozCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/> */}
           <img src={discountedCars[0].carImg} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/>
           <div className={`carousel-caption d-none d-md-block ${style.captionWrapper}`}>
@@ -67,16 +67,18 @@ const Carousel = () => {
           </div>
         </div>
         <div className="carousel-item">
-          {/* <div className={style.overlay}></div> */}
-          <img src={"../assets/carousel/ChevroletCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/1D4PT5GK0BW487259")}/>
+          <div className={style.overlay}></div>
+          {/* <img src={"../assets/carousel/PanozCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/> */}
+          <img src={discountedCars[1].carImg} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/>
           <div className={`carousel-caption d-none d-md-block ${style.captionWrapper}`}>
           <h5>{discountedCars[1].make} {discountedCars[1].model} {discountedCars[1].year}</h5>
             <p className="text-danger" >{formatSum(discountedCars[1].discountedprice())}</p>
           </div>
         </div>
         <div className="carousel-item">
-          {/* <div className={style.overlay}></div> */}
-          <img src={"../assets/carousel/OldsmobileCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUVT68E95A768929")}/>
+          <div className={style.overlay}></div>
+          {/* <img src={"../assets/carousel/PanozCarousel.jpg"} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/> */}
+          <img src={discountedCars[2].carImg} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={()=>history.push("/details/WAUKF98E25A286122")}/>
           <div className={`carousel-caption d-none d-md-block ${style.captionWrapper}`}>
           <h5>{discountedCars[2].make} {discountedCars[2].model} {discountedCars[2].year}</h5>
             <p className="text-danger" >{formatSum(discountedCars[2].discountedprice())}</p>
