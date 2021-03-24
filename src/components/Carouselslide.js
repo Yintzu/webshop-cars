d5c6a279451d8ddbc035b76d62338925579d7857
Chris <to.chris.lindstrom@gmail.com>
2021-03-24 12:02:11 +0100

    testing sale sign render

--- src/components/Carouselslide.js
+++ src/components/Carouselslide.js
@@ -4,6 +4,7 @@ import { CarContext } from "../contexts/CarContext"
 import { ShoppingCartContext } from "../contexts/ShoppingCartContext"
 import { useContext } from "react";
 import style from "../css/DiscountCarousel.module.css";
+import Salesign from './Salesign';
 
 const Carouselslide =()=> {
     const history = useHistory();
@@ -13,10 +14,12 @@ const Carouselslide =()=> {
     return (   
         <div>
             {discountedCars.map((discountedcar,index) => ( 
-                <div className={`carousel-item ${index===0&& 'active' ? "active" : ""}`} key={discountedcar.vin}>
+                <div className={`carousel-item ${index === 0 ? "active" : ""}`} key={discountedcar.vin}>
                     <div className={style.overlay}></div>
                     <img src={discountedcar.carImg} className={`d-block w-100 ${style.carouselImg}`} alt="Discounted Carousel" onClick={() => history.push(`/details/${discountedcar.vin}`)} />
                     <div className={`${style.carouselcaptioned}  d-md-block ${style.captionWrapper}`}>
+                        {/* <p>{`${100 - discountedcar.discount * 100} %`}</p> */}
+                        {/* <Salesign /> */}
                         <h5 className={`${style.carTitle}`}>{discountedcar.make} {discountedcar.model} {discountedcar.year}</h5>
                         <p className={`${style.carPrice}`}>{checkCarDiscount(discountedcar)}</p>
                     </div>
--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -104,9 +104,7 @@ const CarContextProvider = (props) => {
     const checkCarDiscount = (car) => {
       let discountedCar = discountedCars.find(discountCar => discountCar.vin === car.vin);
     
-      return discountedCar ? <span>
-        <span style={{color: 'red'}}>{formatSum(discountedCar.discountedprice())}</span>
-      </span> : formatSum(car.price);
+      return discountedCar ? <span style={{color: 'red'}}>{formatSum(discountedCar.discountedprice())}</span> : formatSum(car.price);
     }
     
     /* Direction to  details page */
