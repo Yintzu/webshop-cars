c09a22dba828ecfec25b08247f158de01dbf4d08
Victor Werngren <victorlillowerngren@gmail.com>
2021-03-24 16:58:44 +0100

    Made thumbnails appear on correct car
    Co-authored-by: Tricia Hartmann <tricia.hartmann@medieinstitutet.se
    >

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -7,21 +7,21 @@ import BuyButtons from '../components/BuyButtons';
 import DetailsModal from '../components/DetailsModal';
 import { waitForElement } from '@testing-library/dom';
 
-    
+
 
 
 const Details = (props) => {
 
     const { cars, boughtCheck, checkCarDiscount } = useContext(CarContext);
     const [car, setCar] = useState(null);
-    const { formatSum} = useContext(ShoppingCartContext);
-    const [ showModal, setShowModal]= useState(false);
-    
-    const modalPicsChevy = [ "chevy-front.jpg", "chevy-interior.jpg", "chevy-rear.jpg" ]
+    const { formatSum } = useContext(ShoppingCartContext);
+    const [showModal, setShowModal] = useState(false);
 
-    const modalPicsOlds =  [ "olds-front.jpg", "olds-interior.jpeg", "olds-rear.jpg" ]
+    const modalPicsChevy = ["chevy-front.jpg", "chevy-interior.jpg", "chevy-rear.jpg"]
 
-    const modalPicsPanoz = [ "panoz-engine.jpg", "panoz-interior.jpg", "panoz-rear.jpg" ]
+    const modalPicsOlds = ["olds-front.jpg", "olds-interior.jpeg", "olds-rear.jpg"]
+
+    const modalPicsPanoz = ["panoz-engine.jpg", "panoz-interior.jpg", "panoz-rear.jpg"]
 
 
     useEffect(() => {
@@ -40,10 +40,17 @@ const Details = (props) => {
         }
     }
 
+    const renderThumbnails = () => {
+        if (props.match.params.id == "1D4PT5GK0BW487259") return modalPicsChevy
+        if (props.match.params.id == "WAUVT68E95A768929") return modalPicsOlds
+        if (props.match.params.id == "WAUKF98E25A286122") { return modalPicsPanoz }
+        else { return [] }
+    }
+
     const renderCar = () => {
         return (
             <div className={style.details}>
-                
+
                 <h1 className={`mt-0 ${style.mainHeading}`}>Car details</h1>
                 <div className="row g-0">
                     <div className={`col ${style.imageWrapper}`}>
@@ -64,7 +71,7 @@ const Details = (props) => {
                         <h5>Description</h5>
                         <p>{car.descLong}</p>
                     </div>
-                  
+
                     <div className="col-md-4">
                         <ul>
                             <li>Make: {car.make}</li>
@@ -75,13 +82,13 @@ const Details = (props) => {
                         {/* <Modal /> */}
                         {showModal && <DetailsModal setShowModal={setShowModal} />}
                     </div>
-                   <div className={style.detailThumbnails}>
-                       { modalPics.map( pic =>
-                        <div className={style.thumbnailWrapper}>
-                           <img src={`../assets/app-components/discount-detail/${pic}`} className={style.thumbnailImg} alt="car detail" />
-                        </div>
-                       )}
-                   </div>
+                    <div className={style.detailThumbnails}>
+                        {renderThumbnails().map((pic, index) =>
+                            <div className={style.thumbnailWrapper} key={index}>
+                                <img src={`../assets/app-components/discount-detail/${pic}`} className={style.thumbnailImg} alt="car detail" />
+                            </div>
+                        )}
+                    </div>
                 </div>
                 <button onClick={() => setShowModal(!showModal)}>Show Modal</button>
             </div>
