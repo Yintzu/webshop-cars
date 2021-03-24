89e07a2cdebb71b94a510db17f6518de39e1c66d
Lisa <lisastina.hansson@gmail.com>
2021-03-24 11:02:59 +0100

    Added NoPurchases component that displays on profile page

---
+++ src/components/NoPurchases.js
@@ -0,0 +1,21 @@
+import style from '../css/NoPurchases.module.css';
+import { NavLink } from "react-router-dom";
+
+const NoPurchases = () => {
+    return ( 
+        <div className={style.noPurchases}>
+            <div>
+                <img src="/assets/app-components/logo.gif" alt="car logo"/>
+                <h4>No purchase history</h4>
+                
+                <p>You have not purchased any cars yet. <br/> Explore our great car deals and check back here after your next shopping trip.</p>
+                <NavLink exact to="/">
+                    <button className="btn">Explore our great deals!</button>
+                </NavLink>
+                
+                </div>
+        </div>
+     );
+}
+ 
+export default NoPurchases;
--- src/components/ProfileCar.js
+++ src/components/ProfileCar.js
@@ -2,44 +2,50 @@ import styles from '../css/ProfileCar.module.css'
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { ShoppingCartContext } from '../contexts/ShoppingCartContext';
+import NoPurchases from '../components/NoPurchases'
 
 const ProfileCar = () => {
 
-  const { orderInfo, loggedInUser } = useContext(UserContext);
+  const { loggedInUser } = useContext(UserContext);
   const { formatSum } = useContext(ShoppingCartContext)
 
   return (
     <div>
       <h1 className={styles.profileCarHeading}>My purchase history</h1>
-      {/* Loopa out orders */}
-      {loggedInUser.orders.map((order, i) => {
-        return (
-          <div key={i}>
-            <div className={styles.orderWrapper}>
-              <div className={styles.orderInfo}>
-                <p className={`${styles.profileCarSubHeading}`}>Order number: {order.orderNumber}</p>
-                <p className={`${styles.profileCarSubHeading}`}>Date: {order.orderDate[0]} {order.orderDate[1]}</p>
-              </div>
+      
+      {/* Check if there's a purchase history */}
+      {loggedInUser.orders.length ? 
+      <div>
+        {/* Loop out orders */}
+        {loggedInUser.orders.map((order, i) => {
+          return (
+            <div key={i}>
+              <div className={styles.orderWrapper}>
+                <div className={styles.orderInfo}>
+                  <p className={`${styles.profileCarSubHeading}`}>Order number: {order.orderNumber}</p>
+                  <p className={`${styles.profileCarSubHeading}`}>Date: {order.orderDate[0]} {order.orderDate[1]}</p>
+                </div>
 
-              {/* Loopa out bought cars */}
-              {order.boughtCars.map((car) => {
-                return (
-                  <div className={styles.flexWrapper} key={car.vin}>
-                    <div className={styles.flexItem}><img src={car.carImg} alt="Car picture" className={`${styles.profileImage}`} /></div>
-                    <div className={`${styles.flexItem}`}>{car.make} {car.model} {car.year}</div>
-                    <div className={`${styles.flexItem2}`}>{formatSum(car.price)}</div>
-                  </div>
-                )
-              })}
+                {/* Loop out bought cars */}
+                {order.boughtCars.map((car) => {
+                  return (
+                    <div className={styles.flexWrapper} key={car.vin}>
+                      <div className={styles.flexItem}><img src={car.carImg} alt="Car picture" className={`${styles.profileImage}`} /></div>
+                      <div className={`${styles.flexItem}`}>{car.make} {car.model} {car.year}</div>
+                      <div className={`${styles.flexItem2}`}>{formatSum(car.price)}</div>
+                    </div>
+                  )
+                })}
 
-              <div className={styles.orderTotal}>
-                <div className={styles.orderTotalText}>Total price: {formatSum(order.price)}</div>
+                <div className={styles.orderTotal}>
+                  <div className={styles.orderTotalText}>Total price: {formatSum(order.price)}</div>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      })}
-
+          )
+        })}
+      </div> 
+      : <NoPurchases/> }
     </div>
   );
 }
